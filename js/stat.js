e1532575e4681bc1fdb0da95473ebd1cd1a12ae0
Andrey <a.ka144@mail.ru>
2019-09-17 02:38:17 +0300

    Добавил var каждой переменной

--- js/stat.js
+++ js/stat.js
@@ -1,14 +1,14 @@
 'use strict';
 
-var CLOUD_X = 100,
-  CLOUD_Y = 10,
-  CLOUD_WIDTH = 420,
-  CLOUD_HEIGHT = 270,
-  GAP = 10,
-  FONT_GAP = 16,
-  MAX_BAR_HEIGHT = 150,
-  BAR_WIDTH = 40,
-  BAR_GAP = 50;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var GAP = 10;
+var FONT_GAP = 16;
+var MAX_BAR_HEIGHT = 150;
+var BAR_WIDTH = 40;
+var BAR_GAP = 50;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
